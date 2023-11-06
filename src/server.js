d2015bb0e7741f9acb6d516e6302c7fc8cb3265b
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-11-06 19:21:40 +0700

    add function to run the server with a response

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const http = require('http');
+
+const port = 9000;
+const host = 'localhost';
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+
+    response.statusCode = 200;
+    response.end('<h1>Halo HTTP Server!</h1>');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+    console.log(`Server is running on port: ${port}`);
+})
