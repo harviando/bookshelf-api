5419e42b6db6c1005464ea8bd87a13c67ac0d027
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-11-07 19:08:37 +0700

    set add book handler

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,38 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
 const addBookHandler = (request, h) => {
-    const { title, author, body } = request.payload;
+    const { name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            reading,
+        } = request.payload;
+
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const newBook = {
+        id,
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        finished: pageCount === readPage,
+        reading,
+        insertedAt,
+        updatedAt,
+    };
+
+    books.push(newBook);
 };
 
 module.exports = { addBookHandler };
