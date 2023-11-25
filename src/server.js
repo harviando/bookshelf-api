2bfc8fc890f6d614876312fc33307989b2d798fe
Replit user <>
2023-11-25 13:24:15 +0000

    init replit and adjust host and port address for hosting

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
-        port: 9000,
-        host: 'localhost',
+        port: process.env.PORT,
+        host: '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
